c33099f6860e03afe2957afe23f4121c82d6ff8f
Angga Gantira <angga.gantira@gmail.com>
2021-01-29 14:52:07 +0700

    25 User Enpoint Dan Auth Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Screencast\PlaylistController;
 use App\Http\Controllers\Screencast\VideoController;
 use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', MeController::class);
+});
 
 Route::prefix('playlists')->group(function() {
     Route::get('', [PlaylistController::class, 'index']);
